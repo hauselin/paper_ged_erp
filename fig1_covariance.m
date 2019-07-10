166ef2b67fa7c07dbb51913ccc9373bd8c37367b
Hause Lin <hauselin@gmail.com>
2019-07-09 22:46:54 -0400

    add comments

--- fig1_covariance.m
+++ fig1_covariance.m
@@ -6,31 +6,32 @@ nchan = 5;
 ntime = 8;
 ntrials = 12;
 
-simdat = randn([nchan,ntime, ntrials]) * 1.5;
+simdat = randn([nchan,ntime, ntrials]) * 1.5; % simulate data
 
+% compute covariance of each trial
 simdat_cov_trials = zeros(nchan,nchan,ntrials);
 for t=1:ntrials
     simdat_cov_trials(:,:,t) = cov(simdat(:,:,t)');
 end
-simdat_cov = mean(simdat_cov_trials,3);
-rank(simdat_cov)
+simdat_cov = mean(simdat_cov_trials,3); % mean covariance
+rank(simdat_cov) % rank of data
 
-simdaterp = mean(simdat,3);
-simdaterp_cov = cov(simdaterp');
+simdaterp = mean(simdat,3); % compute erp
+simdaterp_cov = cov(simdaterp');  % covariance of erp
 rank(simdaterp_cov)
 
 %% plotting
 
 figure(1); clf
 subplot(241)
-trial2plot = randi(ntrials);
-plot(1:ntime, simdat(:,:,trial2plot) + [nchan*5:-5:1]','linew',3)
+trial2plot = randi(ntrials);  % randomly select one trial to plot
+plot(1:ntime, simdat(:,:,trial2plot) + [nchan*5:-5:1]','linew',3) % scale data so data from different channels don't overlap
 xlabel('Time'); ylabel('Channel activity'); title(['Trial ' num2str(trial2plot)])
 xlim([1 ntime]); yticks(''); ylim([1,nchan*5+4])
 axis square
 
 subplot(245)
-imagesc(cov(simdat(:,:,trial2plot)'))
+imagesc(cov(simdat(:,:,trial2plot)')) % covariance of trial
 xlabel('Channel'); ylabel('Channel'); title(['Trial ' num2str(trial2plot) ' covariance'])
 % colorbar
 axis square
@@ -72,8 +73,5 @@ xlabel('Channel'); ylabel('Channel'); title('Mean covariance')
 % colorbar
 axis square
 
-
 % colormap magma % set colour map
-
-saveas(gcf, './figures/Fig01_covariance.png')
-
+% saveas(gcf, './figures/Fig01_covariance.png')
